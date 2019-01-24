fb6b5ecffe0114154e69f3d8ccb4fe84e4a49f5e
Tim Rozmajzl <tim.rozmajzl@gmail.com>
2019-01-24 11:21:22 -0500

    fixed schema

--- 02_ingest/zip_to_csv.sh
+++ 02_ingest/zip_to_csv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo ${YEAR:=2015}  # default if YEAR not set
+echo ${YEAR:=2017}  # default if YEAR not set
 for month in `seq -w 1 12`; do 
    unzip $YEAR$month.zip
    mv *ONTIME.csv $YEAR$month.csv
